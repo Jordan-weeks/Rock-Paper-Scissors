0d2b47b715e29e900a6fe4173e63da5d592d4fa1
Jordan <jdweeks2010@gmail.com>
2022-03-13 20:32:44 -0700

    Added .html and .js to repositorry

---
+++ RPS.js
@@ -0,0 +1,45 @@
+// computer choses rock/paper/scissors
+function computerPlay (){
+    let computerChoices = ["rock","paper","scissors"];
+    let computer= computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    return(computer);
+}
+
+// user choses rock/paper/scissors
+function userPlay(){
+   {
+var choice = prompt("Rock, Paper, or Scissors");
+var choice = choice.toLowerCase();
+}}
+
+// determine winner
+function playRound(computer, user, ){
+ 
+
+if(user == "rock" & computer == "scissors"){ console.log("You win!") + userScore ++
+}
+else if(user == "paper" & computer == "rock"){ console.log("You win!") + userScore ++
+}
+else if(user == "scissors" & computer == "paper"){ console.log("You win!") + userScore ++
+}
+else if(user === computer){console.log("Draw") }
+else{ console.log("you lose!") + computerScore ++}
+console.log (`The score in now computer:${computerScore} user:${userScore}`)
+}
+
+function playGame(){
+    
+    
+while (computerScore < 5 && userScore <5){
+    let computerSelection = computerPlay();
+    let userSelection = userPlay();
+console.log(`Computer choses ${computerSelection}`)
+playRound(computerSelection, userSelection)
+
+}
+}
+
+// declare score
+let computerScore = 0
+let userScore = 0
+playGame()
